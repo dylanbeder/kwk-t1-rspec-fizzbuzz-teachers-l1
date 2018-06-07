cb48031fcaaf8a4e33a0b47bda382601dee38c04
dylanbeder <dylan.beder@duke.edu>
2018-06-07 00:19:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
-  if int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
-  end
-  if int % 15 == 0 
+  elsif int % 15 == 0 
     "FizzBuzz"
-  end
-  if int % 4 == 0 
+  elsif int % 4 == 0 
     nil
   end
 end
